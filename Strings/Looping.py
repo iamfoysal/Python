1424f5899da5d4020f1931e76e2fcf48ff7832a6
foysal <foysalf652@gmail.com>
2022-02-12 01:48:32 +0600

    strings

---
+++ Strings/Looping.py
@@ -0,0 +1,9 @@
+for x in "String":
+    print(x)
+
+print("\n")    #line brack
+
+a= "Python"
+
+for x in a:
+    print(x)
---
+++ Strings/Updating_Strings.py
@@ -0,0 +1,11 @@
+var1 = "Hello Foysal!!"
+var2 = "Python is Best"
+
+
+print(len(var1)) #var1 range or len 
+
+print("\n String Update : \n", var1[:6]+ "Python")
+
+print(len(var2)) #var2 range or len 
+
+print("\n String Updating line 2 :\n", var2[:9] + " Programming Language")
