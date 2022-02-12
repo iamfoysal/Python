61f76b909193649ea1e51b3e428acdedd92e0def
foysal <foysalf652@gmail.com>
2022-02-12 20:01:50 +0600

    Format - Strings

---
+++ Strings/String-Format.py
@@ -0,0 +1,14 @@
+#Python - Format Strings
+
+
+
+name= "My name is wick, and I am {} "
+age="25"
+print(name.format(age))
+
+
+##
+myinfo= "My name is {}, and I am {} "
+me= "Foysal"
+birth= 40
+print(myinfo.format(me, birth)
