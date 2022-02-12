104eba3fdf544459c689d53190443ed1893a61d0
foysal <foysalf652@gmail.com>
2022-02-12 19:21:13 +0600

    check string

---
+++ Strings/Check_String.py
@@ -0,0 +1,42 @@
+#Check String
+
+
+somthing = "this is string, i find text in this somthing variable"
+
+print("not" in somthing)
+
+# this code o/p return False 
+
+somthings = "this is string, i find text in this somthing variable"
+
+print("this" in somthings)
+# this code o/p return True
+
+
+
+# ----------------------------#
+
+#using if else condation
+
+thing = "this is string, i find text in this somthing variable"
+if "free" in thing:
+      print("Yes, 'free' is present.")
+else:
+    print("OHH No!! 'free' in not here ")
+
+
+
+####
+
+this = "this is string, i find text in this somthing variable"
+if "find" in this:
+      print("Yes, 'find' is present.")
+else:
+    print("OHH No!! 'find' in not here ")
+
+###
+
+
+hello= "Python is best programming Language"
+if "free" not in hello:
+      print("can't find!!, 'free'")
