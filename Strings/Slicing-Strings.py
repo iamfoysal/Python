e413aeeaa87d7b9ea86020346f6dcff18b87e45d
foysal <foysalf652@gmail.com>
2022-02-12 19:38:35 +0600

    Slicing Strings

---
+++ Strings/Slicing-Strings.py
@@ -0,0 +1,17 @@
+#Slicing
+
+x = "The first. character has index 0"
+
+print(x[4:10]) ## start 4 no index and stop 9 no index
+
+print(x[4:]) ## start 4 no index and end last
+
+print(x[:10]) ##end 10 no index
+
+
+
+
+##Negative Indexing
+
+y = "Negative Indexing!"
+print(y[-9:-2])
