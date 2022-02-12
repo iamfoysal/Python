c54351a19cbd3dc050b3b995fcae101939c2b627
foysal <foysalf652@gmail.com>
2022-02-12 19:49:08 +0600

    modify string

---
+++ Strings/Modify-Strings.py
@@ -0,0 +1,31 @@
+#Upper Case
+
+text= "Upper Case"
+
+print(text.upper())
+
+
+
+#Lower Case
+
+
+text= "Lower CASE"
+
+print(text.lower())
+
+#Remove Whitespace
+
+text1= "  Remove Whitespace  "
+
+print(text1.strip())
+
+#Replace String
+
+txt2= "Rython"
+print(txt2.replace("R","P"))
+
+
+
+#Split String 
+txt3= "Python! is Best"
+print(txt3.split("!"))
